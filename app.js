4c0f17dcefc08eabfe4ea1c6ab319880a3dc526a
Perry Sittser <sittserp@gmail.com>
2020-10-12 16:21:52 -0700

    random pokemon images

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 // import functions and grab DOM elements
+import pokedex from './assets/pokedex.js';
+
+const pokemon1 = document.getElementById('pokemon1');
+const pokemon2 = document.getElementById('pokemon2');
+const pokemon3 = document.getElementById('pokemon3');
 
 // initialize state
+
+const randomIndex1 = Math.floor(Math.random() * 14);
+const randomIndex2 = Math.floor(Math.random() * 14);
+const randomIndex3 = Math.floor(Math.random() * 14);
+
+pokemon1.src = pokedex[randomIndex1].url_image;
+pokemon2.src = pokedex[randomIndex2].url_image;
+pokemon3.src = pokedex[randomIndex3].url_image;
+
+
+
 
 // set event listeners to update state and DOM
