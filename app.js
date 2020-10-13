28909f15920a88f843b79aef1ff31a1bb01474a7
Perry Sittser <sittserp@gmail.com>
2020-10-13 10:45:47 -0700

    fix broken while loops

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const pokemon3 = document.getElementById('pokemon3');
 // initialize state
 
 let catchCounter = 0;
-const caught = [];
+export const caught = [];
 
 function setupPokemon() {
 
     catchCounter++;
+    console.log(catchCounter);
+    console.log(caught);
 
     if (catchCounter === 11) {
         window.location.href = 'results.html';
@@ -20,24 +22,26 @@ function setupPokemon() {
 
     let randomIndex1 = Math.floor(Math.random() * 14);
     let randomIndex2 = Math.floor(Math.random() * 14);
-    let randomIndex3 = Math.floor(Math.random() * 14);
-
-    pokemon1.src = pokedex[randomIndex1].url_image;
-    pokemon1.id = pokedex[randomIndex1].id;
-
-    pokemon2.src = pokedex[randomIndex2].url_image;
-
-    while (pokemon2.src === pokemon1.src) {
+    while (randomIndex2 === randomIndex1) {
         randomIndex2 = Math.floor(Math.random() * 14);
     }
-    pokemon2.id = pokedex[randomIndex2].id;
-
-    pokemon3.src = pokedex[randomIndex3].url_image;
-
-    while (pokemon3.src === pokemon1.src || pokemon3.src === pokemon2.src) {
+    let randomIndex3 = Math.floor(Math.random() * 14);
+    while (randomIndex3 === randomIndex1 || randomIndex3 === randomIndex2) {
         randomIndex3 = Math.floor(Math.random() * 14);
     }
+
+    pokemon1.id = pokedex[randomIndex1].id;
+    pokemon1.src = pokedex[randomIndex1].url_image;
+
+    pokemon2.id = pokedex[randomIndex2].id;
+    pokemon2.src = pokedex[randomIndex2].url_image;
+
     pokemon3.id = pokedex[randomIndex3].id;
+    pokemon3.src = pokedex[randomIndex3].url_image;
+
+    console.log(randomIndex1);
+    console.log(randomIndex2);
+    console.log(randomIndex3);
 
 }
 
---
+++ results.js
@@ -0,0 +1,2 @@
+import { caught } from 'app.js';
+
