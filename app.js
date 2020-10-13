7378080bec67e1bee16d106f0fb384b5009f8ce8
Perry Sittser <sittserp@gmail.com>
2020-10-12 17:14:12 -0700

    click listeners for new trio and caught-array

--- app.js
+++ app.js
@@ -7,15 +7,50 @@ const pokemon3 = document.getElementById('pokemon3');
 
 // initialize state
 
-const randomIndex1 = Math.floor(Math.random() * 14);
-const randomIndex2 = Math.floor(Math.random() * 14);
-const randomIndex3 = Math.floor(Math.random() * 14);
 
-pokemon1.src = pokedex[randomIndex1].url_image;
-pokemon2.src = pokedex[randomIndex2].url_image;
-pokemon3.src = pokedex[randomIndex3].url_image;
+function setupPokemon() {
 
+    let randomIndex1 = Math.floor(Math.random() * 14);
+    let randomIndex2 = Math.floor(Math.random() * 14);
+    let randomIndex3 = Math.floor(Math.random() * 14);
 
+    pokemon1.src = pokedex[randomIndex1].url_image;
+    pokemon1.id = pokedex[randomIndex1].id;
 
+    pokemon2.src = pokedex[randomIndex2].url_image;
+
+    while (pokemon2.src === pokemon1.src) {
+        randomIndex2 = Math.floor(Math.random() * 14);
+    }
+    pokemon2.id = pokedex[randomIndex2].id;
+
+    pokemon3.src = pokedex[randomIndex3].url_image;
+
+    while (pokemon3.src === pokemon1.src || pokemon3.src === pokemon2.src) {
+        randomIndex3 = Math.floor(Math.random() * 14);
+    }
+    pokemon3.id = pokedex[randomIndex3].id;
+
+}
+
+setupPokemon();
 
 // set event listeners to update state and DOM
+
+const caught = [];
+
+pokemon1.addEventListener('click', () => {
+    caught.push(pokemon1.id);
+    setupPokemon();
+});
+
+pokemon2.addEventListener('click', () => {
+    caught.push(pokemon2.id);
+});
+
+pokemon3.addEventListener('click', () => {
+    caught.push(pokemon3.id);
+});
+
+console.log(caught);
+
